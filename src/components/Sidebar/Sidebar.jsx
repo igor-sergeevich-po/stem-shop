699c92c16d5299145067aee4cf384058780734ac
Igor_PO <89515076779@ya.ru>
2023-03-09 19:34:41 +0300

    add sidebar layout

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,5 +1,40 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import styles from "../../styles/sidebar.module.scss";
 
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>categories</div>
+
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`/categories/${1}`}>shorts</NavLink>
+          </li>
+          <li>
+            <NavLink to={`/categories/${2}`}>boots</NavLink>
+          </li>
+          <li>
+            <NavLink to={`/categories/${2}`}>nike</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <a href="/help" className={styles.link} target="_blank">
+          help
+        </a>
+
+        <a
+          href="/terms"
+          style={{ textDecoration: "underline" }}
+          className={styles.link}
+          target="_blank"
+        >
+          Terms & Conditions
+        </a>
+      </div>
+    </section>
+  );
 };
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,4 +1,5 @@
 export const ROUTES = {
   home: "/",
   cart: "cart",
+  category: "/categoies/:id",
 };
