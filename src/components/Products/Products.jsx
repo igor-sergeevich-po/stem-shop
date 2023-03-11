86e5e2c32f6a28ca2783a84082f67c3ff634d7ae
Igor_PO <89515076779@ya.ru>
2023-03-11 16:27:47 +0300

    add product layout

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "../../styles/products.module.scss";
+
+export const Products = ({ title, style = {}, products = [], amount }) => {
+  const list = products.filter((_, i) => i < amount);
+
+  return (
+    <section className={style.products} style={style}>
+      {title && <h2>{title}</h2>}
+
+      <div className={styles.list}>
+        {list.map(({ id, images, title, category: { name: cat }, price }) => (
+          <Link className="styles.product" to={`/products/${id}`} key={id}>
+            <div
+              className={styles.image}
+              style={{ backgroundImage: `url(${images[0]})` }}
+            />
+            <div className={styles.wrapper}>
+              <h3 className={styles.title}>{title}</h3>
+              <div className={styles.cat}>{cat}</div>
+              <div className={styles.info}>
+                <div className={styles.prices}>
+                  <div className={styles.price}>{price} Ру</div>
+                  <div className={styles.oldPrice}>{Math.floor(price * 0.8)} Ру</div>
+                </div>
+              </div>
+              <div className={styles.purchases}>
+                {Math.floor(Math.random() * 15 + 3)} purchased
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
+};
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -2,4 +2,5 @@ export const ROUTES = {
   home: "/",
   cart: "cart",
   category: "/categoies/:id",
+  product: "/products/:id",
 };
