e25691a8fbe1dedaa91b1d7c4761c434a9c20dfb
Igor_PO <89515076779@ya.ru>
2023-03-11 16:46:50 +0300

    slice: add productsSlice

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,17 +2,21 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 import { getCategories } from "../../redux/slices/categoriesSlice";
+import { getProducts } from "../../redux/slices/productsSlice";
+
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
 import { AppRoutes } from "../Routes/Routes";
-
 import { Sidebar } from "../Sidebar/Sidebar";
 
 function App() {
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getCategories());
+    dispatch(getProducts());
   }, [dispatch]);
+
   return (
     <div className="App">
       <Header />
---
+++ src/redux/slices/productsSlice.js
@@ -0,0 +1,41 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+import { BASE_URL } from "../../utils/constants";
+
+const initialState = {
+  list: [],
+  isLoading: false,
+};
+
+export const getProducts = createAsyncThunk(
+  "products/getProducts",
+  async (_, thunkAPI) => {
+    try {
+      const res = await axios(`${BASE_URL}/products`);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const productsSlice = createSlice({
+  name: "products",
+  initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getProducts.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getProducts.fulfilled, (state, { payload }) => {
+      state.list = payload;
+      state.isLoading = false;
+    });
+    builder.addCase(getProducts.rejected, (state) => {
+      state.isLoading = false;
+    });
+  },
+});
+
+export default productsSlice.reducer;
